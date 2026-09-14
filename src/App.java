aefb03747ca54a92850ac46c4dc4937d10997fc4
TE24b-Camen-Sixten <sixtencamen@icloud.com>
2026-09-14 11:10:50 +0200

    Klar med boka och omsättning

--- src/App.java
+++ src/App.java
@@ -1,28 +1,48 @@
-void main(){
-    Boolean[] platser = new Boolean[20];
+void main() {
+    boolean[] platser = new boolean[20];
     Meny(platser);
 }
 
-static void Meny(Boolean[] platser){
+static void Meny(boolean[] platser) {
     IO.println("1. Lägg till en bokning\n2. Visa platser\n3. Visa omsättning\n4. Avsluta");
-    String val = IO.readln("Vad vill du göra (1-4): ");
-    
+    String val = IO.readln("Vad vill du göra? (1-4): ");
+    IO.println();
+
     int tras;
     switch (val) {
         case "1" -> Boka(platser);
         case "2" -> Inehåll(platser);
-        case "3" -> Omsättning();
+        case "3" -> Omsättning(platser);
         case "4" -> tras = 1 / 0;
         default -> IO.println("du är korkad");
     }
 }
 
-static void Boka(Boolean[] platser){
-    String platsString = IO.readln("Vilken plats ska bokas? ");
+static void Boka(boolean[] platser) {
+    String platsString = IO.readln("Vilken plats ska bokas? (1-20): ");
+    int platsInt = Integer.parseInt(platsString) - 1; // Fixar så det inte är sämst senare
+
+    if (!platser[platsInt]) {
+        platser[platsInt] = true;
+        IO.println("Nu är plats " + (platsInt + 1) + " bokad");
+    } else {
+        IO.println("Den platsen är upptagen");
+    }
 }
-static void Inehåll(Boolean[] platser){
-    for (Boolean plats : platser) {
+
+static void Inehåll(boolean[] platser) {
+    for (boolean plats : platser) {
         IO.print(plats + " ");
     }
 }
-static void Omsättning(){}
+
+static void Omsättning(boolean[] platser) {
+    int omsättning = 0;
+    for (boolean plats : platser) {
+        if (plats) {
+            omsättning += 300;
+        }
+    }
+
+    IO.println(omsättning);
+}
